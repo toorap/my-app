fa417503081ecc1f451af7a3ffda9d06959c64b6
Parshotam Toora <toorap@TMGM-C02T62PLFVH4.local>
2018-02-14 20:37:47 +0000

    first pass

---
+++ my-app/src/main/java/com/mycompany/app/CalculateRevenue.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import java.math.BigDecimal;
+
+public class CalculateRevenue implements RevenueCalculator
+{
+    public CalculateRevenue() {
+
+    }
+
+    public  BigDecimal calculateRevenue(BigDecimal marginPercentage, BigDecimal costOfGoods) {
+
+        BigDecimal percentageDecimal = marginPercentage.divide(new BigDecimal(100));
+        BigDecimal revenuePercentage = new BigDecimal(1).subtract(percentageDecimal);
+        BigDecimal revenue = costOfGoods.divide(revenuePercentage);
+        return revenue;
+    }
+}
---
+++ my-app/src/main/java/com/mycompany/app/RevenueCalculator.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+/**
+ * Created by toorap on 14/02/2018.
+ */
+import java.math.BigDecimal;
+
+public interface RevenueCalculator {
+
+    BigDecimal calculateRevenue(
+
+            BigDecimal marginPercentage,
+
+            BigDecimal costOfGoods
+
+    );
+
+}
